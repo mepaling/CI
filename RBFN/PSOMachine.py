d3192488d95bdeb8f817ba5a885b4ed699d59493
mepaling <dick50414@gmail.com>
2017-04-26 06:27:55 +0800

    remove some sensitive words

--- RBFN/PSOMachine.py
+++ RBFN/PSOMachine.py
@@ -5,9 +5,9 @@
 class PSOMachine(object):
     """Particle Swarm Optimization Machine - the UI and base settings of PSO Algo"""
     def __init__(self):
-        print "I'm the fucking Particle Swarm Optimization Machine!"
+        print "I'm the Particle Swarm Optimization Machine!"
 
     def main(self, sensorx, sensory, sensorz):
         """Get the theta value by sensor value"""
-        print "Get the fucking theta"
+        print "Get the theta value"
         
